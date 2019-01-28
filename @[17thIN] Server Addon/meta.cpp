ffad15e76d8d8c4d6eac45521d45475f1928e4d9
Badger <llamaorman4@gmail.com>
2019-01-28 16:35:16 +0000

    Initial Upload

---
+++ @[17thIN] Server Addon/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 1308232858;
+name = "[10thACB] Ghost Addon";
+timestamp = 5248235403257339070;
---
+++ @[17thIN] Server Addon/mod.cpp
@@ -0,0 +1,14 @@
+name = "17thIN: Server Addon";
+description = "17thIN: Server Addon";
+picture = "17thIN_Insignia.paa";
+actionName = "Website";
+//action = "http://www.10thacb.com";
+
+logo = "17thIN_Insignia.paa";
+logoOver = "17thIN_Insignia.paa";
+logoSmall = "17thIN_Insignia.paa";
+
+tooltip = "17thIN: Server Addon";
+tooltipOwned = "17thIN: Server Addon - Owned";
+overview = "Ghost Addon with additional usage for 17thIN Systems<br/><br/>www.website.com";
+author = "17thIN";
